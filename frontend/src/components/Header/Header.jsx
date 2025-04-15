09eb786977aa3ca25b3a432989469ac29077a23d
s1mple0315 <casscassovich@gmail.com>
2025-04-15 17:14:48 +0500

    Refactor code structure for improved readability and maintainability

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
